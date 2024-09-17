86aed2398af93132a22e10f1d450c341763d711a
Cemil Şahin <cemil.sahin@ozu.edu.tr>
2024-09-18 00:36:08 +0300

    add expandable (vertically) text input

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './MainPage.css'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
@@ -8,7 +8,7 @@ function MainPage() {
         <div id='main-page'>
             <div id='chat-block'>
                 <div id='input-block'>
-                    <input className='text-input' type="text" />
+                    <ExpandableMessageBox/>
                     <button id='input-button'>
                         <i className="fa-solid fa-paper-plane fa-lg"></i>
                     </button>
@@ -20,3 +20,29 @@ function MainPage() {
   
 export default MainPage;
   
+
+const ExpandableMessageBox: React.FC = () => {
+    const [value, setValue] = useState('');
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+    // Adjust the height of the textarea based on its content
+    useEffect(() => {
+        if (textareaRef.current) {
+            textareaRef.current.style.height = 'auto';
+            textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+            if (textareaRef.current.scrollHeight > 250) {
+                textareaRef.current.style.height = '250px'
+            }
+        }
+    }, [value]);
+
+    return (
+        <textarea
+            ref={textareaRef}
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            placeholder="Type your message here..."
+            className="expandable-textarea"
+        />
+    );
+};
