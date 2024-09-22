77ce86cb8fc5771d76755ae55276aa1bdd20da34
Cemil Şahin <cemil.sahin@ozu.edu.tr>
2024-09-22 23:32:58 +0300

    add key enter & button functionality

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,13 +3,39 @@ import './MainPage.css'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
-function MainPage() {
+interface ExpandableMessageBoxProps {
+    value: string;
+    setValue: (value: string) => void;
+    textareaRef: React.RefObject<HTMLTextAreaElement>; // Accept the ref as a prop
+}
+
+const MainPage: React.FC = () => {
+    const [value, setValue] = useState(''); 
+    const textareaRef = useRef<HTMLTextAreaElement>(null);
+
+    const HandleSendButton = () => {
+        const currentValue = textareaRef.current?.value;
+        const trimmedValue = currentValue?.trim();
+        console.log(textareaRef.current?.value)
+        if (trimmedValue) {
+            const currentValue = textareaRef.current?.value;
+            console.log(trimmedValue);
+            setValue(''); 
+            textareaRef.current!.value = '';
+            console.log(currentValue);
+        }
+    };
+
     return (
         <div id='main-page'>
             <div id='chat-block'>
                 <div id='input-block'>
-                    <ExpandableMessageBox/>
-                    <button id='input-button'>
+                    <ExpandableMessageBox
+                        value={value}
+                        setValue={setValue}
+                        textareaRef={textareaRef}
+                    />
+                    <button id='input-button' onClick={HandleSendButton}>
                         <i className="fa-solid fa-paper-plane fa-lg"></i>
                     </button>
                 </div>
@@ -21,9 +47,21 @@ function MainPage() {
 export default MainPage;
   
 
-const ExpandableMessageBox: React.FC = () => {
-    const [value, setValue] = useState('');
-    const textareaRef = useRef<HTMLTextAreaElement>(null);
+const ExpandableMessageBox: React.FC<ExpandableMessageBoxProps> = ({ value, setValue, textareaRef }) => {
+    
+    const HandleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+        const currentValue = textareaRef.current?.value;
+        const trimmedValue = currentValue?.trim();
+
+        if (event.key === 'Enter' && (!trimmedValue || trimmedValue === '')) {
+            event.preventDefault();
+        } else if (event.key === 'Enter' && trimmedValue) {
+            console.log(currentValue);
+            setValue('');
+            event.preventDefault();
+        }    
+    }
+
     
     // this works for a line having 48 characters
     useEffect(() => {
@@ -47,6 +85,7 @@ const ExpandableMessageBox: React.FC = () => {
             ref={textareaRef}
             value={value}
             onChange={(e) => setValue(e.target.value)}
+            onKeyDown={HandleKeyDown}
             placeholder="Type your message here..."
             className="expandable-textarea"
         />
